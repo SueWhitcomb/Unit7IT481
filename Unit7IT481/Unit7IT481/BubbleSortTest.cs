e3dee52cb6c325ed1d98e8e83f6dae70977596a6
SueWhitcomb <39161955+SueWhitcomb@users.noreply.github.com>
2018-05-20 16:10:37 -0500

    Add files via upload

---
+++ Unit7IT481/Unit7IT481/BubbleSortTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class BubbleSortTest
+{
+    public static void Main(string[] args)
+    {
+        int[] numbers = new int[500];
+        BubbleSortTrial obj = new BubbleSortTrial();
+
+        obj.ReadFromFile(numbers);
+
+        Console.Write("Unsorted: ");
+
+
+
+
+        obj.DisplayArray(numbers);
+
+
+
+        obj.BubbleSortArray(numbers);
+
+        Console.Write("Sorted: ");
+
+        obj.DisplayArray(numbers);
+
+        Console.ReadLine();
+    }
+}
---
+++ Unit7IT481/Unit7IT481/BubbleSortTrial.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+
+
+public class BubbleSortTrial
+{          
+
+
+    public BubbleSortTrial()
+    {
+    }
+    public void ReadFromFile(int[] numbers)
+    {
+        string path = "c:\\users\\sue\\source\\repos\\Unit7IT481\\Unit7IT481\\numbers.txt";
+        StreamReader sr = new StreamReader(path);
+
+        for (int index = 0; index < numbers.Length; index++)
+        {
+            numbers[index] = Convert.ToInt32(sr.ReadLine());
+
+        }
+        sr.Close();
+    }
+    public void BubbleSortArray(int[] numbers)
+    {
+        bool swap;
+        int temp;
+
+        do
+        {
+            swap = false;
+            for (int index = 0; index < (numbers.Length - 1); index++)
+            {
+                if (numbers[index] > numbers[index + 1])
+                {
+                    temp = numbers[index];
+                    numbers[index] = numbers[index + 1];
+                    numbers[index + 1] = temp;
+                    swap = true;
+
+                }
+            }
+        } while (swap == true);
+
+    }
+
+    public void DisplayArray(int[] numbers)
+    {
+        for (int index = 0; index < numbers.Length; index++)
+        {
+            Console.Write("{0}", numbers[index]);
+        }
+    }
+}
+
